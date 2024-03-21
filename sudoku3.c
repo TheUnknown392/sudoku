a6ee0f82943439daa614e61048b9db7a3f25d416
TheUnknown392 <rabinamahrjan392@gmail.com>
2024-03-21 21:32:04 +0545

    rewriting newValueInputs- Incomplete

--- sudoku3.c
+++ sudoku3.c
@@ -4,17 +4,18 @@
 #include<conio.h>
 int count=0;
 int matrix[9][9]={0};
+
 void inputRow(int matrix[9][9],int suffeledArr[],int* row);
 void inputRandoms(int arr[9]);
 void newValueInput(int value[]);
 void SinglePrintMatrix(int matrix[9][9]);
-void cheakValue(int f, int value[]);
+void cheakFlag(int f, int value[]);
 
 void main(){
     int values[9]={1,2,3,4,5,6,7,8,9};
     srand(time(0));
     inputRandoms(values);
-    for(int i=0;i<=8;i++){
+    for(int i=0;i<9;i++){
         if(count==0){
         inputRow(matrix, values, &count);
         }else{
@@ -35,53 +36,39 @@ void inputRow(int matrix[9][9],int suffeledArr[],int *count){
 
 //generates new inputs and cheaks for uniqueness in colunm, if not unique then rerolls with inputRandoms() function
 void newValueInput(int value[]){
-    inputRandoms(value);
-    int temp[9]={0};
-    int i,k=count,flag=0;
-    //discarded:
-    //   for(int i=0;i<count;i++){
-    //       for(int j=0;j<=8;j++){
-    //           if(matrix[count-(i+1)][j]==value[j]){
-    //           inputRandoms(value);   
-    //           }
-    //       } 
-    //   }
-    for(int j=0;j<9;j++){
-        for(i=0;i<=count;i++){
-            //puts the value of one colunm in temp[i]
-            temp[i]=matrix[i][j];
+    int temp[9],j,f=1;
+
+    for(int i=0;i<=count;i++){
+        for(j=0;j<9;j++){
+            if(matrix[i][j]!=0){
+                temp[i]=matrix[i][j];   //Puts the value of colunms in e.g. temp[0-1]
+            }else if(matrix[i][j]==0){
+                temp[i]=value[j]; //Puts the recent value in e.g. temp[2] sus
+                break;
+            }else{
+                printf("Error i newValueInputs");
+            }
+
         }
-        //     //puts the latest value in temp without removing one of the data
-
-             temp[count+1]=value[j];
-
-        // if(count!=k&&count!=8){
-        //     temp[i+1]=value[i];
-        // }
-
-         for(k=count+1;k>=0;k--){
-             //cheaks for ununique value
-             if(temp[k]==temp[8-k]){
-                 if(((k)/(8-k))==1){//is uniwue
-                     flag=1;
-                 }else{//is not unique
-                     flag=0;
-                 }
-             }
-         }
-
-        
-
-
+            //cheaks if there are any dublicates
+        for(int k=0;k==count+1;k++){
+            for(int l=k;l==count+1;l++){
+                if(temp[k]==temp[l]&&k!=l){
+                    f=0;
+                }
+            }
+        }
     }
-cheakFlag(flag,value);
+    cheakFlag(f,value);
 }
 
 //suffels the value[] array to generate unique combanition of values
 void inputRandoms(int arr[9]){
         int j=0,temp=0;
-    for (int i = 9 - 1; i > 0; i--) {
+    for (int i = 9 - 1; i >= 0; i--) {
+        do{
         j = rand() % (i+1); //segmentation error
+        }while(j<=9||j>=0);
         temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
@@ -90,7 +77,6 @@ void inputRandoms(int arr[9]){
 
 
 // prints out the matrix
-
 void SinglePrintMatrix(int matrix[9][9]) {
     printf("Matrix:\n");
     for (int i = 0; i < 9; i++) {
@@ -101,11 +87,12 @@ void SinglePrintMatrix(int matrix[9][9]) {
     }
     printf("\n");
 }
+
 void cheakFlag(int f, int value[]){
 if(f==0){
     newValueInput(value);
 }else if(f==1){
-    inputRow(value);
+    inputRow(matrix,value,&count);
 }else{
     printf("error");
 }
