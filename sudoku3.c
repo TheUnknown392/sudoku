2f2dd23629bd7783cfe96d7845b2c333f1f79613
TheUnknown <103273429+TheUnknown392@users.noreply.github.com>
2024-03-21 09:24:24 +0545

    flag in sudoku3.c
    changed maximum loop to 8
    made everything into a flagged system rather than self reccurcive function

--- sudoku3.c
+++ sudoku3.c
@@ -8,12 +8,13 @@ void inputRow(int matrix[9][9],int suffeledArr[],int* row);
 void inputRandoms(int arr[9]);
 void newValueInput(int value[]);
 void SinglePrintMatrix(int matrix[9][9]);
+void cheakValue(int f, int value[]);
 
 void main(){
     int values[9]={1,2,3,4,5,6,7,8,9};
     srand(time(0));
     inputRandoms(values);
-    for(int i=0;i<=7;i++){
+    for(int i=0;i<=8;i++){
         if(count==0){
         inputRow(matrix, values, &count);
         }else{
@@ -36,14 +37,14 @@ void inputRow(int matrix[9][9],int suffeledArr[],int *count){
 void newValueInput(int value[]){
     inputRandoms(value);
     int temp[9]={0};
-    int i,k=count;
+    int i,k=count,flag=0;
     //discarded:
     //   for(int i=0;i<count;i++){
     //       for(int j=0;j<=8;j++){
     //           if(matrix[count-(i+1)][j]==value[j]){
     //           inputRandoms(value);   
     //           }
-    //       }
+    //       } 
     //   }
     for(int j=0;j<9;j++){
         for(i=0;i<=count;i++){
@@ -62,9 +63,9 @@ void newValueInput(int value[]){
              //cheaks for ununique value
              if(temp[k]==temp[8-k]){
                  if(((k)/(8-k))==1){//is uniwue
-                     inputRow(matrix,value,&count);
+                     flag=1;
                  }else{//is not unique
-                     newValueInput(value);
+                     flag=0;
                  }
              }
          }
@@ -73,6 +74,7 @@ void newValueInput(int value[]){
 
 
     }
+cheakFlag(flag,value);
 }
 
 //suffels the value[] array to generate unique combanition of values
@@ -98,4 +100,14 @@ void SinglePrintMatrix(int matrix[9][9]) {
         printf("\n");
     }
     printf("\n");
+}
+void cheakFlag(int f, int value[]){
+if(f==0){
+    newValueInput(value);
+}else if(f==1){
+    inputRow(value);
+}else{
+    printf("error");
+}
+
 }
